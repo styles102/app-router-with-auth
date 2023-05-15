f9eabd381a35ad1dfc94564f2d13e1c8df1fae80
Liam Styles <liam.styles@distinction.co.uk>
2023-05-15 11:23:24 +0100

    get session specific data

--- app/(auth)/my-account/page.tsx
+++ app/(auth)/my-account/page.tsx
@@ -1,9 +1,15 @@
-export default function MyAccount() {
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth"
+
+export default async function MyAccount() {
+	const session = (await getServerSession(authOptions))!;
+
 	return (
 		<main>
 			<h1>My Account Page</h1>
 			<div>
-				This page requires auth.
+				This page requires auth. <br />
+				Currently logged in as {session.user!.name}
 			</div>
 		</main>
 	)
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,27 @@
 import NextAuth from "next-auth";
 import Auth0Provider from "next-auth/providers/auth0";
+import type { NextAuthOptions } from "next-auth";
 import { ENV } from "@/lib/env";
 
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
 	providers: [
 		Auth0Provider({
 			clientId: ENV.AUTH0_CLIENT_ID,
 			clientSecret: ENV.AUTH0_CLIENT_SECRET,
 			issuer: ENV.AUTH0_ISSUER_BASE_URL
 		})
-	]
-})
+	],
+	session: {
+		strategy: "jwt"
+	},
+	callbacks: {
+		async session({ session, token, user }) {
+			return session;
+		}
+	}
+}
+
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }
